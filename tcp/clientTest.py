c477bfaf3eab40ad291c7d91ef330c0f684b15a6
TayKaiJun <tehohno@gmail.com>
2023-10-25 17:37:50 -0700

    lab3-mqtt

--- tcp/clientTest.py
+++ tcp/clientTest.py
@@ -1,7 +1,7 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('YOUR_SERVER_IP', 8080))
+client.connect(('164.67.214.47', 8080))
 client.sendall('I am CLIENT\n'.encode())
 from_server = client.recv(4096)
 client.close()
